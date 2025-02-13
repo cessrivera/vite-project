57135977fe827e5d597a7d8dbb19bb7c61d5de5c
GeeksforGeeks <GFGexample@gmail.orgg>
2025-02-13 12:41:50 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>PRINCESS DIANE RIVERA</h1>
+      <div
+        style={{
+          width: "200px",
+          height: "200px",
+          backgroundColor: "pink",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          margin: "0 auto",
+          border: "2px solid black",
+        }}
+      >
+        <p>CPEITEL</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
